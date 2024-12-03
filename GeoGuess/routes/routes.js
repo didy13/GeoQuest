feb0cec0a0ada194821b4addef2edec3b7a876c9
Stefke006 <10221stefansofronijevic@etsntesla.edu.rs>
2024-12-03 12:58:37 +0100

    Dodate rute za log in i log out takodje i sesija koja se pravi tokom log in-a i proverava prilikom ulaska na index

--- GeoGuess/routes/routes.js
+++ GeoGuess/routes/routes.js
@@ -1,11 +1,71 @@
 const express = require("express");
-
+const session = require("express-session");
 const router = express.Router();
 const connection = require("../controller/config");
 
-router.get("/", (req,res)=>{
-    res.render("index", {title: "Home"});
-})
+const crypto = require('crypto');
 
+const secretKey = crypto.randomBytes(64).toString('hex');
+router.use(session({
+    secret: secretKey,  
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }  
+}));
+
+
+const isAuthenticated = (req, res, next) =>
+{
+    if (req.session.user) {
+        next(); 
+    } else {
+        res.redirect("/login"); 
+    }
+}
+
+
+router.get("/", isAuthenticated, (req, res) => {
+    res.render("index", { title: "Home", user: req.session.user });
+});
+
+
+router.get("/login", (req, res) => {
+    if (req.session.user) {
+        return res.redirect("/"); 
+    }
+    res.render("login");
+});
+
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const query = "SELECT * FROM Korisnik WHERE nickname = ? AND lozinka = ?";
+
+    connection.query(query, [username, password], (err, results) => {
+        if (err) {
+            return res.status(500).send("Internal Server Error");
+        }
+
+        if (results.length > 0) {
+            req.session.user = {
+                id: results[0].id,
+                username: results[0].username
+            };
+            res.redirect("/");
+        } else {
+            res.render("login", { error: "Invalid username or password" });
+        }
+    });
+});
+
+
+router.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send("Unable to log out");
+        }
+        res.redirect("/login");
+    });
+});
 
 module.exports = router;
