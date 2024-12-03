a7d60a406f9c247078348f23c01fd19b04255a6d
Stefke006 <10221stefansofronijevic@etsntesla.edu.rs>
2024-12-03 11:06:27 +0100

    Instalirani neophodni node moduli i izmenjene sitne sintaksne greske

--- GeoGuess/app.js
+++ GeoGuess/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const routes = require("./routes/products_routes");
+const routes = require("./routes/routes");
 const dotenv = require("dotenv");
 dotenv.config();
 
