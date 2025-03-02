ac1690f7d541008c0798b38c737062959e0d7c80
David Koloski <davidkoloski30@gmail.com>
2025-03-02 23:46:11 +0100

    Pokusaj 100

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const routes = require("./routes/routes");
+const routes = require("../routes/routes");
 const dotenv = require("dotenv");
 dotenv.config();
 
